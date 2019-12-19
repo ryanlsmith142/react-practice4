619c1b8077cf5797bbd58021c59535fa1ecdec17
Ryan Smith <ryan.l.smith142@gmail.com>
2019-12-19 07:57:39 -0600

    work with regular React without JSX and then added JSX

--- src/index.js
+++ src/index.js
@@ -7,6 +7,43 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// const title = React.createElement(
+//     'h1',
+//     { id: 'main-title' },
+//     'My first React Element!'
+// );
+
+// const title = 'My First React Element!';
+const myTitleID = 'main-title';
+const name = 'Ryan';
+// const desc = React.createElement(
+//     'p',
+//     null,
+//     'I just learned how to create a React node and render it into the DOM.'
+// );
+
+const desc = 'I just learned how to create a React node and render it into the DOM.';
+
+// const header = React.createElement(
+//   'header',
+//   null,
+//   title,
+//   desc
+// );
+
+const header = (
+    <header>
+        <h1 id={ myTitleID }>{ name }'s First React Element!</h1>
+        <p>{ desc }</p>
+    </header>
+);
+
+ReactDOM.render(
+    header,
+    document.getElementById('treehouse')
+);
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
